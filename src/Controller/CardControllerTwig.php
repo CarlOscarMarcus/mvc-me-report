9fe0d1c4b888f6d9fe9f7b1b1b326696f8ab03dd
Oscar Wystråle (osay21) <osay21@student.bth.se>
2023-04-11 18:13:57 +0200

    card navigation

---
+++ src/Controller/CardControllerTwig.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Controller;
+
+use App\Deck\Deck;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class CardControllerTwig extends AbstractController
+{
+
+    #[Route("/card", name: "card")]
+    public function card(): Response
+    {
+        $deck = new Deck();
+        $deck->sort();
+        $data = [
+            'deck' => $deck->cardsToString()
+        ];
+
+        return $this->render('cards/index.html.twig');
+    }
+
+    // #[Route("/deck", name: "deck")]
+    // public function deck(): Response
+    // {
+    //     $deck = new Deck();
+    //     $deck->sort();
+    //     $data = [
+    //         'deck' => $deck->cardsToString()
+    //     ];
+
+    //     return $this->render('cards/deck.html.twig', $data);
+    // }
+}
